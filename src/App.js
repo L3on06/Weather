bac8549a5be13f5d511cb6bb283d93b3a9b708b2
L3on06 <l3onshabani@gmail.com>
2022-10-01 21:55:21 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [bg, setbg] = useState(true);
+
+  const active = () => {
+    setbg(!bg)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="body" style={{ backgroundColor: bg ? '' : '#212121', color: bg ? 'black' : 'white' }}>
+      <header className='header container flex'>
+        <div className='logo'>
+          <a className='logoName' href='/' style={{ color: bg ? 'black' : 'white' }}><h1>Weather</h1></a>
+          <img className='logoImg' src="./images/cloud.png" alt='logo' />
+        </div>
+        <nav className='navbar'>
+          <div onClick={active}>{(bg)
+            ? <img className='modeImg' src="./images/day.png" alt='day-mode' />
+            : <img className='modeImg' src="./images/night.png" alt='night-mode' />}
+          </div>
+        </nav>
       </header>
+
+      <main className=' main container'>
+        <div>
+          <h1>leon</h1>
+        </div>
+      </main>
+
+
     </div>
   );
 }
