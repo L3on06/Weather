c57f5b18ca1be109dedbd56fb290ab188223b36f
L3on06 <l3onshabani@gmail.com>
2022-10-03 09:56:33 -0700

    added img for each weather conditions,anime search

--- src/App.js
+++ src/App.js
@@ -5,50 +5,64 @@ import WeatherWidget from './Components/WeatherWidget';
 
 
 function App() {
-const api_key = '5dbfe70ca7194ddab81195717220110';
-const api_url = 'http://api.weatherapi.com/v1';
-const api_method = '/current.json'
+  const api_key = '5dbfe70ca7194ddab81195717220110';
+  const api_url = 'http://api.weatherapi.com/v1';
+  const api_method = '/current.json'
   const api = `${api_url}${api_method}?key=${api_key}&q=`
-  
 
-  const [search, setSearch] = useState();
+
+  // const [search, setSearch] = useState();
   const [data, setData] = useState();
-  const [bg, setbg] = useState(true);
+  const [bg, setBg] = useState(true);
 
 
   const active = () => {
-    setbg(!bg)
+    setBg(!bg)
   }
 
   const handleSearch = (e) => {
     e.preventDefault();
-    setSearch(e.target.value)
-    console.log(e.target[0].value);
+    // setSearch(e.target.value)
+    // console.log(e.target[0].value);
 
     axios.get(`${api}${e.target[0].value}`)
-          .then(response => setData(response.data))
-          .catch(error => console.log(error))
-          e.target.value = ''
-
-  //   switch (e.keyCode) {
-  //     case 13:
-  //       axios.get(`${api}${search}`)
-  //         .then(response => setData(response.data))
-  //         .catch(error => console.log(error))
-  //         e.target.value = ''
-  //       break;
-  //     default:
-  //   }
+      .then(response => setData(response.data))
+      .catch(error => console.log(error))
+    e.target[0].value = '';
   }
-  
+
 
   return (
     <div className="body" style={{ backgroundColor: bg ? '' : '#212121', color: bg ? '#212121' : 'white' }}>
       <header className='header container flex'>
         <div className='logo'>
           <a className='logoName' href='/' style={{ color: bg ? '#212121' : 'white' }}><h1>Weather</h1></a>
-          <img className='logoImg' src="./images/cloud.png" alt='logo' />
+          <img className='logoImg' src="./images/bigCloud_smallSun.png" alt='logo' />
         </div>
+        <form className='form-control' onSubmit={handleSearch}>
+            <input type="value" required=""/>
+              <label>
+                <span style={{ transitionDelay: '0ms' }}>S</span>
+                <span style={{ transitionDelay: '50ms' }}>e</span>
+                <span style={{ transitionDelay: '100ms' }}>a</span>
+                <span style={{ transitionDelay: '150ms' }}>r</span>
+                <span style={{ transitionDelay: '200ms' }}>c</span>
+                <span style={{ transitionDelay: '250ms' }}>h</span>
+                <span style={{ transitionDelay: '300ms' }}></span>
+                <span style={{ transitionDelay: '350ms' }}>C</span>
+                <span style={{ transitionDelay: '400ms' }}>o</span>
+                <span style={{ transitionDelay: '450ms' }}>u</span>
+                <span style={{ transitionDelay: '500ms' }}>t</span>
+                <span style={{ transitionDelay: '550ms' }}>r</span>
+                <span style={{ transitionDelay: '600ms' }}>y</span>
+                <span style={{ transitionDelay: '650ms' }}>/</span>
+                <span style={{ transitionDelay: '700ms' }}>c</span>
+                <span style={{ transitionDelay: '750ms' }}>i</span>
+                <span style={{ transitionDelay: '800ms' }}>t</span>
+                <span style={{ transitionDelay: '850ms' }}>y</span>
+              </label>
+        </form>
+
         <nav className='navbar'>
           <div onClick={active}>{(bg)
             ? <img className='modeImg' src="./images/day.png" alt='day-mode' />
@@ -56,15 +70,10 @@ const api_method = '/current.json'
           </div>
         </nav>
       </header>
-
       <main className='main container'>
         <div>
-          
-          <form  onSubmit={handleSearch}>
-          <input className='search' type="search" placeholder="Search Country/City" />
-
-          </form>
-          {data && <WeatherWidget data={data} />}
+        
+          {data && <WeatherWidget bg={bg} data={data} />}
         </div>
       </main>
 
--- src/Components/WeatherWidget.js
+++ src/Components/WeatherWidget.js
@@ -1,34 +1,60 @@
 import React from "react";
 import '../App.css';
 
-function WeatherWidget({ data }) {
+function WeatherWidget( props ){
     const handleIcon = () => {
         var url = ''
-        switch (data.current.condition.text) {
+        switch (props.data.current.condition.text) {
+            case "Clear" && "Sunny":
+                url = './images/sun.png'
+                break;
             case "Partly cloudy":
-                url = './images/day.png'
+                url = './images/bigSun_smallCloud.png'
+                break;
+            case "Cloudy":
+                url = './images/Cloudy.png'
+                break;
+            case "Overcast":
+                url = './images/bigCloud_smallSun.png'
+                break;
+            case "Fog" || "Mist" || "Freezing fog":
+                url = './images/fog.png'
+                break;
+            case "Patchy rain possible" && "Patchy sleet possible":
+                url = './images/cloud_rain.png'
+                break;
+            case "Patchy snow possible" && "Patchy freezing drizzle possible" && "Blowing snow" && "Light snow" && 'Patchy moderate snow' && 'Moderate snow' && 'Patchy heavy snow' && 'Heavy snow' && 'Ice pellets' && 'Light snow showers' && 'Moderate or heavy snow showers' && 'Light showers of ice pellets' && 'Moderate or heavy showers of ice pellets' && 'Patchy light snow with thunder' && 'Moderate or heavy snow with thunder':
+                url = './images/cloud_snow.png'
+                break;
+            case "Thundery outbreaks possible":
+                url = './images/sun.png'
+                break;
+            case "Patchy light drizzle" && "Light drizzle" && "Freezing drizzle" && "Heavy freezing drizzle" && "Patchy light rain" && "Light rain" && "Moderate rain at times" && "Moderate rain" && "Heavy rain at times" && 'Heavy rain' && 'Light freezing rain' && 'Moderate or heavy freezing rain' && 'Light sleet' && 'Moderate or heavy sleet' && 'Patchy light snow' && 'Light rain shower' && 'Moderate or heavy rain shower' && 'Torrential rain shower' && 'Light sleet showers' && 'Moderate or heavy sleet showers':
+                url = './images/sun.png'
+                break;
+            case "Patchy light rain with thunder" && "Moderate or heavy rain with thunder":
+                url = './images/Cloudy.png'
                 break;
             default:
-                url = './images/day.png'
-                break;
         }
-                return url;
+        return url;
     }
-    console.log(data.current.condition.text)
-    console.log(data)
+    const leon = props.bg;
+    console.log(leon)
 
     return (
-        <div className="widget">
-            <h1 className="widget-location">{data.location.country}/{data.location.name}</h1>
-            <img className="widget-icon" src={handleIcon()} alt={data.current.condition.text}></img>
+        <div className="widget" style={{boxShadow: leon ? '-22px -22px 40px #63a9e6, 22px 22px 40px #79cfff': "-31px -31px 62px #1b1b1b, 31px 31px 62px #272727"}}>
+            <h1 className="widget-location">{props.data.location.country}/{props.data.location.name}</h1>
+            <img className="widget-icon" src={handleIcon()} alt={props.data.current.condition.text}></img>
             <h1 className="widget-c">
-                {data.current.temp_c}<sup>&bull;</sup>C
+                {props.data.current.temp_c}<sup>&bull;</sup>C
             </h1>
             <p className="widget-text">
-                {data.current.condition.text}
+                {/* <img src={data.current.condition.icon} alt='log2'></img> */}
+                {props.data.current.condition.text}
             </p>
             <p className="widget-time">
-                {data.location.localtime}
+                {props.data.location.localtime}
             </p>
         </div>
     )
